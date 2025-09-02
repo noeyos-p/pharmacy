9ce2b1e2e1b731af7aee93d4e4d07b6d7277bfbf
noeyos-p <krapnoeyos@gmail.com>
2025-09-02 12:06:07 +0900

    카카오 api에서 받을 dto rntjd

---
+++ src/main/java/com/my/pharmacy/dto/MetaDto.java
@@ -0,0 +1,16 @@
+package com.my.pharmacy.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class MetaDto {
+    @JsonProperty("total_count")
+    /* json key, total_count를 totalCount로 받음 */
+    private Integer totalCount;
+
+
+}
